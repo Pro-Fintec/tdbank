229022e6db92537116208714804aab5d458bc7b3
Joana Agyemang <jagyemang7@icloud.com>
2022-02-24 21:55:33 +0000

    my first commit

---
+++ app.java
@@ -0,0 +1,7 @@
+v1
+       echo "Please enter your name"
+       read name
+       echo "Please enter your password"
+       read -s password  
+       echo "Welcome to the zelle application"
+       -------
